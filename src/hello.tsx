742166a5be49780317490820f5362d4fa00582c6
pixel <303176530@qq.com>
2019-10-17 15:25:30 +0800

    s展开配置项

--- src/hello.tsx
+++ src/hello.tsx
@@ -14,18 +14,18 @@ class Hello extends React.Component<IProps,IState> {
   count:1,
   test:0
  }
- public tick:() => void = ()=>{
+ public tick:(a:any) => void = (a:any)=>{
    this.setState({count:this.state.count+1})
  }
  public inFun:(a:number)=>void=(a:number)=>{
   this.setState({test:a})
  }
   public render(){
+    const a = <button onClick={()=>{this.inFun(3)}}>{this.state.test}</button>
     return(
       <div>
         Hello {this.props.name}
-        <button onClick={this.tick}>{this.state.count}</button>
-        <button onClick={()=>{this.inFun(3)}}>{this.state.test}</button>
+        <button onClick={()=>{this.tick(a)}}>{this.state.count}</button>
       </div>
     )
   }
